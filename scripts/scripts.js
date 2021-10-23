c065e4fae199ece614025c96688d1ca290c123d3
Siou <abc8631916@gmail.com>
2021-10-23 10:37:02 +0800

    E_Wha_korean BBQ Version_01

---
+++ scripts/scripts.js
@@ -0,0 +1,8 @@
+const FB=document.getElementById("FB")
+FB.addEventListener("click" , function(){
+    window.open("https://www.facebook.com/%E6%A2%A8%E7%87%92%E4%BA%94%E8%8A%B1%E9%9F%93%E5%BC%8F%E7%87%92%E8%82%89-101328371421725/")
+})
+const IG=document.getElementById("IG")
+IG.addEventListener("click" , function(){
+    window.open("https://www.instagram.com/e_wha_korea_bbq/")
+})
